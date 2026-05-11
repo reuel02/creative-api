d32332fe6e90bff6464ed3353efadc74e136a703
Reuel Ferreira <ferreirareuel3@gmail.com>
2026-05-11 17:12:50 -0300

    Estrutura do projeto

---
+++ app/core/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+# URL de conexão com o banco de dados PostgreSQL usando asyncpg
+DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/meubanco"
+
+engine = create_async_engine(DATABASE_URL)
+AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+Base = declarative_base()
+
